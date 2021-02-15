c66fe6a33f4f6c6f8171afc8de871be0cb30fade
szczep4niak <jjdroid6969@gmail.com>
2021-02-15 13:43:59 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
+cd backend
+npm run build
+cd ..
+mv backend/public_nodejs .
+
 cd app
 npm run build
 cd ..
-mv app/build backend/src
-cd backend
-npm run build
+mv app/build public_nodejs/src
+
+
