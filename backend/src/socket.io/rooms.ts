143ff22abad9cea79983cb6df368df264b469669
szczep4niak <jjdroid6969@gmail.com>
2021-03-15 15:19:06 +0100

    redis error handling

--- backend/src/socket.io/rooms.ts
+++ backend/src/socket.io/rooms.ts
@@ -9,58 +9,60 @@ const registerRoomHandlers = (io: Server, socket: Socket) => {
   socket.on('room:create', async () => {
     const roomId = nanoid(6);
     await socket.join(roomId);
-    console.log('room:create', socket.id);
-
-    console.log(socket.rooms);
-
     socket.emit('room:joined', roomId);
   });
 
   socket.on('room:join', async (roomId: string) => {
-    console.log('room:join', socket.id);
+    try {
+      const room = io.sockets.adapter.rooms.get(roomId);
+      const isJoinable = room && room.size === 1;
 
-    const room = io.sockets.adapter.rooms.get(roomId);
-    const isJoinable = room && room.size === 1;
+      if (!room || !isJoinable) return socket.emit('room:missing');
 
-    if (!room || !isJoinable) return socket.emit('room:missing');
+      const roomFromDbString = await Redis.asyncGet(roomId);
+      const isRejoin = Boolean(roomFromDbString);
 
-    const roomFromDbString = await Redis.asyncGet(roomId);
-    const isRejoin = Boolean(roomFromDbString);
+      await socket.join(roomId);
+      if (!isRejoin) socket.emit('room:joined', roomId);
 
-    await socket.join(roomId);
-    if (!isRejoin) socket.emit('room:joined', roomId);
+      const roomToDb = isRejoin
+        ? getUpdatedRoom(roomFromDbString as string, room, socket.id)
+        : await getNewRoom(room);
 
-    const roomToDb = isRejoin
-      ? getUpdatedRoom(roomFromDbString as string, room, socket.id)
-      : await getNewRoom(room);
+      Redis.client.setex(roomId, 60 * 60, JSON.stringify(roomToDb));
 
-    Redis.client.setex(roomId, 60 * 60, JSON.stringify(roomToDb));
-
-    const meal: Meal = await getMealToSend(roomToDb, socket);
-    const target = isRejoin ? socket : io.in(roomId);
-    target.emit('room:meals', meal);
+      const meal: Meal = await getMealToSend(roomToDb, socket);
+      const target = isRejoin ? socket : io.in(roomId);
+      target.emit('room:meals', meal);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   socket.on('room:vote', async (vote: boolean) => {
-    const roomId = socket.rooms.keys().next().value;
-    const roomFromDb = await Redis.get(roomId);
-    const voteList = [...roomFromDb[socket.id], vote];
-    const roomToDb = { ...roomFromDb, ...{ [socket.id]: voteList } };
+    try {
+      const roomId = socket.rooms.keys().next().value;
+      const roomFromDb = await Redis.get(roomId);
+      const voteList = [...roomFromDb[socket.id], vote];
+      const roomToDb = { ...roomFromDb, ...{ [socket.id]: voteList } };
 
-    const isVoteAMatch = isMatch(roomToDb, voteList.length - 1);
+      const isVoteAMatch = isMatch(roomToDb, voteList.length - 1);
 
-    console.log(roomFromDb.meals[voteList.length]);
+      console.log(roomFromDb.meals[voteList.length]);
 
-    Redis.client.setex(roomId, 60 * 60, JSON.stringify(roomToDb));
-    if (isVoteAMatch) {
-      const mealDetails: MealDetails = await fetchMealDetails(
-        roomFromDb.meals[voteList.length - 1]
-      );
-      return io.in(roomId).emit('room:match', mealDetails);
+      Redis.client.setex(roomId, 60 * 60, JSON.stringify(roomToDb));
+      if (isVoteAMatch) {
+        const mealDetails: MealDetails = await fetchMealDetails(
+          roomFromDb.meals[voteList.length - 1]
+        );
+        return io.in(roomId).emit('room:match', mealDetails);
+      }
+
+      const nextMeal = await Redis.get(roomFromDb.meals[voteList.length]);
+      socket.emit('room:meals', nextMeal);
+    } catch (err) {
+      console.log(err);
     }
-
-    const nextMeal = await Redis.get(roomFromDb.meals[voteList.length]);
-    socket.emit('room:meals', nextMeal);
   });
 };
 
@@ -88,7 +90,6 @@ const getUpdatedRoom = (
 
 const getNewRoom = async (room: Set<string>): Promise<Room> => {
   const userIds = Array.from(room);
-
   const randomMealIds = await getRandomMeals();
   return {
     meals: randomMealIds,
